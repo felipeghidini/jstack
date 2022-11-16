3a0902299756f3082133b60f97a92c91985ea5d9
felipeghidini <lipeghidini18@gmail.com>
2022-11-16 06:40:39 -0300

    Criado as collections

---
+++ api/src/app/models/Category.ts
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+import { model, Schema } from 'mongoose';
+
+export const Category = model('Category', new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  }
+}));
