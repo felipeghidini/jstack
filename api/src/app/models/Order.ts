62fca5c001a86e05b8099cf912b021c87ed3355a
felipeghidini <lipeghidini18@gmail.com>
2022-11-17 22:40:35 -0300

    API Finalizada

--- api/src/app/models/Order.ts
+++ api/src/app/models/Order.ts
@@ -10,7 +10,7 @@ export const Order = model('Order', new Schema({
   status: {
     type: String,
     enum: ['WAITING', 'IN_PRODUCTION', 'DONE'],
-    default: 'WATING',
+    default: 'WAITING',
   },
   createdAt: {
     type: Date,
