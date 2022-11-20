c31a1843ee421d2abc7738ab53c6a62b3ff05382
felipeghidini <lipeghidini18@gmail.com>
2022-11-20 07:06:18 -0300

    Iniciando Orders

---
+++ frontend/src/components/Orders/index.tsx
@@ -0,0 +1,26 @@
+import { Board, Container, OrdersContainer } from './styles';
+
+export function Orders() {
+  return (
+    <Container>
+      <Board>
+        <header>
+          <span>🧭</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+          <button type='button'>
+            <strong>Mesa 2</strong>
+            <span>2 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+    </Container>
+  );
+}
---
+++ frontend/src/components/Orders/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1216px;
+  margin: 40px auto;
+`;
+
+export const Board = styled.div`
+  padding: 16px;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  header {
+    padding: 8px;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+export const OrdersContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  button {
+    background: #fff;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    border-radius: 8px;
+    height: 128px;
+  }
+`;
