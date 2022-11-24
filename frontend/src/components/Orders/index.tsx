d9915b12a840d6c11ef150296f927b305415c4fb
felipeghidini <lipeghidini18@gmail.com>
2022-11-24 15:34:28 -0300

    Criando o objeto de retorno de pedidos

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -1,5 +1,25 @@
+import { Order } from '../../types/Order';
 import { OrdersBoard } from '../OrdersBoard';
 import { Container } from './styles';
+
+const orders: Order[] = [
+  {
+    '_id': '6376dc284a52d4c311831b0b',
+    'table': '123',
+    'status': 'IN_PRODUCTION',
+    'products': [
+      {
+        'product': {
+          'name': 'Coca cola',
+          'imagePath': '1668732418470-coca-cola.png',
+          'price': 7,
+        },
+        'quantity': 1,
+        '_id': '6376dc284a52d4c311831b0c'
+      }
+    ],
+  },
+];
 
 export function Orders() {
   return (
@@ -7,15 +27,18 @@ export function Orders() {
       <OrdersBoard
         icon="🧭"
         title="Fila de espera"
+        orders={orders}
       />
 
       <OrdersBoard
         icon="👩‍🍳"
         title="Em preparação"
+        orders={[]}
       />
       <OrdersBoard
         icon="✅"
         title="Pronto!"
+        orders={[]}
       />
     </Container>
   );
